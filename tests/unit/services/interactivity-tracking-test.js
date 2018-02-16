4d4c1505a20c0e1c15e7ecd7188b77b70d57d01d
Jordan Hawker <hawker.jordan@gmail.com>
2018-02-15 21:24:42 -0800

    Add metrics service to tracking test

--- tests/unit/services/interactivity-tracking-test.js
+++ tests/unit/services/interactivity-tracking-test.js
@@ -1,8 +1,7 @@
 import { moduleFor, test } from 'ember-qunit';
 
 moduleFor('service:interactivity-tracking', 'Unit | Service | interactivity tracking', {
-  // Specify the other units that are required for this test.
-  // needs: ['service:foo']
+  needs: ['service:metrics']
 });
 
 // Replace this with your real tests.
